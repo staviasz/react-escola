136c37f1c2b90b647a2856366721e83b6b9a3281
Erick Staviasz <erickesc1@hotmail.com>
2023-05-28 20:56:54 -0300

    desenvolvimento da page Alunos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import PrivateRoute from './PrivateRoute';
+
 import Login from '../pages/Login';
 import Aluno from '../pages/Aluno';
 import Alunos from '../pages/Alunos';
@@ -20,8 +21,8 @@ export default function Routes() {
         component={Register}
         isClosed={false}
       />
-      <PrivateRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
       <PrivateRoute exact path="/aluno/" component={Aluno} isClosed />
+      <PrivateRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
       <PrivateRoute exact path="/avatar/:id" component={Avatar} isClosed />
       <PrivateRoute path="*" component={Page404} />
     </Switch>
